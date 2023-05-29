064bf4eae20dcb340e0ac96f92004be21215ebb0
floretdemola <floretdemola@gmail.com>
2023-05-29 18:37:19 -0400

    added handlebars

---
+++ public/js/comment.js
@@ -0,0 +1,27 @@
+const commentFormHandler = async function(event){
+    event.preventDefault();
+    
+        const comment = document.querySelector('input[name="post-id"]').value;
+        const body = document.querySelector('textarea[name="comment-body"]').value;
+    
+        await fetch('/api/comment', {
+            method: 'POST',
+            body: JSON.stringify({
+                comment,
+                body
+            }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+    
+    document.location.reload();
+};
+    
+const deleteClickHandler = async function() {
+        await fetch('api/comment', {
+            method: 'DELETE'
+        });
+    document.location.reload();
+};
+    
+document.querySelector('#edit-post-form').addEventListener('submit', commentFormHandler);
+document.querySelector('#delete-btn').addEventListener('click', deleteClickHandler);
---
+++ public/js/login.js
@@ -0,0 +1,51 @@
+const loginFormHandler = async function(event) {
+    event.preventDefault();
+
+    const usernameEl = document.querySelector('#username-input');
+    const passwordEl = document.querySelector('#password-input');
+
+    const response = await fetch('api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: usernameEl.value.trim(),
+            password: passwordEl.value.trim()
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (name && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ name, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+  
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
