73511e12addf3a7055dfbf909f91c43a5832f1fe
floretdemola <floretdemola@gmail.com>
2023-05-30 09:55:03 -0400

    troubleshooting handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -24,31 +24,4 @@ const loginFormHandler = async function(event) {
     }
 };
 
-const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (name && email && password) {
-      try {
-        const response = await fetch('/api/users', {
-          method: 'POST',
-          body: JSON.stringify({ name, email, password }),
-          headers: { 'Content-Type': 'application/json' },
-        });
-    
-        if (response.ok) {
-          document.location.replace('/profile');
-        } else {
-          throw new Error(response.statusText);
-        } 
-      } catch (err){
-        alert(err.message);
-    }}
-  };
-  
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-  
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
---
+++ public/js/signup.js
@@ -0,0 +1,26 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (name && email && password) {
+      try {
+        const response = await fetch('/api/users', {
+          method: 'POST',
+          body: JSON.stringify({ name, email, password }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+    
+        if (response.ok) {
+          document.location.replace('/profile');
+        } else {
+          throw new Error(response.statusText);
+        } 
+      } catch (err){
+        alert(err.message);
+    }}
+  };
+
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
