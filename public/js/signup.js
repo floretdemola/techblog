e4a54efff208a9fb7416ea7ee8fd827db77ab3e9
floretdemola <floretdemola@gmail.com>
2023-05-30 09:57:29 -0400

    troubleshooting handlebars

--- public/js/signup.js
+++ public/js/signup.js
@@ -7,7 +7,7 @@ const signupFormHandler = async (event) => {
   
     if (name && email && password) {
       try {
-        const response = await fetch('/api/users', {
+        const response = await fetch('/api/user', {
           method: 'POST',
           body: JSON.stringify({ name, email, password }),
           headers: { 'Content-Type': 'application/json' },
