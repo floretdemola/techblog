d2fc5a53e48ca0554179845e20db141cdddf0270
floretdemola <floretdemola@gmail.com>
2023-05-30 13:18:02 -0400

    troubleshooting handlebars dashboard and new post

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -42,7 +42,7 @@ router.get('/post/:id', withAuth, async (req, res) => {
   });
 
 router.get('/login', (req, res) => {
-        if (req.session.logged_in) {
+        if (req.session.loggedIn) {
           res.redirect('/');
           return;
         }
@@ -50,7 +50,7 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/signup', (req, res) => {
-    if (req.session.logged_in) {
+    if (req.session.loggedIn) {
       res.redirect('/');
       return;
       }
